6bd3bc374c2cac0b550bcd6192e8f35f6ad7cc92
JenPetSmit <72626008+jenpetsmit@users.noreply.github.com>
2025-03-04 09:49:40 -0500

    Update webcrawlerpython.py

--- docs/webcrawlerpython.py
+++ docs/webcrawlerpython.py
@@ -1,8 +1,4 @@
-layout: page
-title: "Web Crawler Python Script"
-permalink: /docs/webcrawlerpython.py
-
-
+```
 
 # Import all needed libraries 
 # Open the URL and read the API reponse
@@ -34,8 +30,7 @@ with open("youtube_videos_sra.csv", "w", encoding="utf-8") as csv_file:
         else:
             print("no more videos") 
             has_another_page = False
-
-
+```
 
 ---
 
